fd09207e79cddb8de591f1e384794ee383479c9c
Nan-Hung Hsieh <d99622005@ntu.edu.tw>
2024-01-24 13:37:11 -0800

    update

--- lvim/config.lua
+++ lvim/config.lua
@@ -4,6 +4,20 @@
 -- Discord: https://discord.com/invite/Xb9B4Ny
 lvim.plugins = {
   { "jalvesaq/Nvim-R" },
-  { "vim-pandoc/vim-pandoc" },
-  { "vim-pandoc/vim-pandoc-syntax" },
+ -- { "vim-pandoc/vim-pandoc" },
+ -- { "vim-pandoc/vim-pandoc-syntax" },
 }
+vim.cmd [[
+  set relativenumber
+  set colorcolumn=80
+]]
+vim.keymap.set('i', 'jj', "<Esc>", { silent = true })
+
+-- folding powered by treesitter
+-- https://github.com/nvim-treesitter/nvim-treesitter#folding
+-- look for foldenable: https://github.com/neovim/neovim/blob/master/src/nvim/options.lua
+-- Vim cheatsheet, look for folds keys: https://devhints.io/vim
+vim.opt.foldmethod = "indent" -- https://neovim.io/doc/user/fold.html
+vim.opt.foldexpr = "nvim_treesitter#foldexpr()" -- default is ""
+vim.opt.foldenable = true -- if this option is true and fold method option is other than normal, every time a document is opened everything will be folded.
+
