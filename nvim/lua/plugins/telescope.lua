3ef2dd1e00e716e80b251e29adcdfc55ff578da4
Nan-Hung Hsieh <nan_1003@hotmail.com>
2024-11-21 21:20:40 -0800

    update

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -16,8 +16,8 @@ return {
       })
       local builtin = require("telescope.builtin")
       vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
-      vim.keymap.set("n", "<leader>lg", builtin.live_grep, {})
-      vim.keymap.set("n", "<leader>of", builtin.oldfiles, {})
+      vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
+      vim.keymap.set("n", "<leader>fo", builtin.oldfiles, {})
 
       require("telescope").load_extension("ui-select")
     end,
