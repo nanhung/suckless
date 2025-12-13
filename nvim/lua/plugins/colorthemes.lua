785237ce5c5062cc302c10ac26a29c2df47a8816
Nan-Hung Hsieh <nan_1003@hotmail.com>
2025-12-13 10:07:19 -0800

    nvim

--- nvim/lua/plugins/colorthemes.lua
+++ nvim/lua/plugins/colorthemes.lua
@@ -1,8 +1,4 @@
 return {
-  { 'shaunsingh/nord.nvim', enabled = false, lazy = false, priority = 1000 },
-  { 'folke/tokyonight.nvim', enabled = false, lazy = false, priority = 1000 },
-  { 'EdenEast/nightfox.nvim', enabled = false, lazy = false, priority = 1000 },
-  { 'srcery-colors/srcery-vim', enabled = false, lazy = false, priority = 1000 },
   {
     'catppuccin/nvim',
     name = 'catppuccin',
@@ -16,98 +12,5 @@ return {
       vim.api.nvim_set_hl(0, 'Tabline', { fg = colors.green, bg = colors.mantle })
       vim.api.nvim_set_hl(0, 'TermCursor', { fg = '#A6E3A1', bg = '#A6E3A1' })
     end,
-  },
-
-  {
-    'oxfist/night-owl.nvim',
-    enabled = false,
-    lazy = false,
-    priority = 1000,
-    config = function()
-      -- load the colorscheme here
-      require('night-owl').setup()
-      vim.cmd.colorscheme 'night-owl'
-      vim.api.nvim_set_hl(0, 'TermCursor', { fg = '#A6E3A1', bg = '#A6E3A1' })
-    end,
-  },
-
-  {
-    'rebelot/kanagawa.nvim',
-    enabled = false,
-    lazy = false,
-    priority = 1000,
-    config = function()
-      require('kanagawa').setup {
-        colors = {
-          theme = {
-            all = {
-              ui = {
-                bg_gutter = 'none',
-              },
-            },
-          },
-        },
-      }
-      vim.cmd.colorscheme 'kanagawa'
-      vim.api.nvim_set_hl(0, 'TermCursor', { fg = '#A6E3A1', bg = '#A6E3A1' })
-    end,
-  },
-  {
-      "tiagovla/tokyodark.nvim",
-      enabled = false,
-      opts = {
-          -- custom options here
-      },
-      config = function(_, opts)
-          require("tokyodark").setup(opts) -- calling setup is optional
-          vim.cmd [[colorscheme tokyodark]]
-      end,
-  },
-
-  {
-    'olimorris/onedarkpro.nvim',
-    enabled = false,
-    lazy = false,
-    priority = 1000,
-  },
-
-  {
-    'neanias/everforest-nvim',
-    enabled = false,
-    lazy = false,
-    priority = 1000,
-  },
-
-  -- color html colors
-  {
-    'NvChad/nvim-colorizer.lua',
-    enabled = true,
-    opts = {
-      filetypes = { '*' },
-      user_default_options = {
-        RGB = true, -- #RGB hex codes
-        RRGGBB = true, -- #RRGGBB hex codes
-        names = true, -- "Name" codes like Blue or blue
-        RRGGBBAA = true, -- #RRGGBBAA hex codes
-        AARRGGBB = false, -- 0xAARRGGBB hex codes
-        rgb_fn = false, -- CSS rgb() and rgba() functions
-        hsl_fn = false, -- CSS hsl() and hsla() functions
-        css = false, -- Enable all CSS features: rgb_fn, hsl_fn, names, RGB, RRGGBB
-        css_fn = false, -- Enable all CSS *functions*: rgb_fn, hsl_fn
-        -- Available modes for `mode`: foreground, background,  virtualtext
-        mode = 'background', -- Set the display mode.
-        -- Available methods are false / true / "normal" / "lsp" / "both"
-        -- True is same as normal
-        tailwind = false, -- Enable tailwind colors
-        -- parsers can contain values used in |user_default_options|
-        sass = { enable = false, parsers = { 'css' } }, -- Enable sass colors
-        virtualtext = '■',
-        -- update color values even if buffer is not focused
-        -- example use: cmp_menu, cmp_docs
-        always_update = false,
-        -- all the sub-options of filetypes apply to buftypes
-      },
-      buftypes = {},
-    },
   },
 }
--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -3,7 +3,7 @@ return {
     build = ":TSUpdate",
     config = function ()
         require("nvim-treesitter.configs").setup({
-            ensure_installed = { "markdown", "markdown_inline", "r", "rnoweb", "yaml", "csv" },
+            ensure_installed = { "lua", "markdown", "markdown_inline", "r", "rnoweb", "yaml", "csv" },
             highlight = { enable = true },
         })
     end
