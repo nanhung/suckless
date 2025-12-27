2365c275d5674cba45ddbe81c7f033b0a28014fe
Nan-Hung Hsieh <nan_1003@hotmail.com>
2025-12-26 21:20:58 -0800

    update

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,6 +1,7 @@
 return {
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
+    version = "0.10.0",
     config = function ()
         require("nvim-treesitter.configs").setup({
             ensure_installed = { "lua", "markdown", "markdown_inline", "r", "rnoweb", "yaml", "csv" },
