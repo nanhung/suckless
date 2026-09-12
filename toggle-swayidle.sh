6e1d48b76989148eec8f70d2ba7bbd74b41f21ea
Nan-Hung Hsieh <nan_1003@hotmail.com>
2026-09-12 08:39:54 -0700

    update

--- toggle-swayidle.sh
+++ toggle-swayidle.sh
@@ -8,13 +8,16 @@ if pgrep -x swayidle >/dev/null; then
         T*)
             pkill -CONT -x swayidle
             echo "swayidle" "Enabled"
+            notify-send "swayidle" "Enabled"
             ;;
         *)
             pkill -STOP -x swayidle
             echo "swayidle" "Disabled"
+            notify-send "swayidle" "Disabled"
             ;;
     esac
 else
     echo "swayidle" "Not running"
+    notify-send "swayidle" "Not running"
     exit 1
 fi
